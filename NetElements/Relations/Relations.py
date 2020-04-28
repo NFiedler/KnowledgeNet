d99f9bc826175bcd1bdfabb9b7cefd18fc59093d
Niklas Fiedler <git@nfiedler.de>
2020-04-28 15:04:44 +0200

    added probabilistic to relation classes

--- NetElements/Relations/Relations.py
+++ NetElements/Relations/Relations.py
@@ -2,6 +2,7 @@ from typing import List, Dict, Optional
 from bson import ObjectId
 from abc import abstractclassmethod
 
+EPSILON = 0.000000001
 
 class RelationType:
 
@@ -11,6 +12,7 @@ class RelationType:
                  description: str = '',
                  values: Optional[Dict] = None,
                  reflexive: bool = False,
+                 probabilistic: bool = False,
                  relation_type_id: Optional[ObjectId] = None,
                  ):
         self.name = name
@@ -19,6 +21,7 @@ class RelationType:
             values = dict()
         self.values = values
         self.reflexive = reflexive
+        self.probabilistic = probabilistic
         self.is_uni = uni
         self.id = relation_type_id
 
@@ -31,6 +34,7 @@ class RelationType:
                 description=relation_type_dict['description'],
                 values=relation_type_dict['values'],
                 reflexive=relation_type_dict['reflexive'],
+                probabilistic=relation_type_dict['probabilistic'],
                 relation_type_id=relation_type_dict['_id'],
             )
         return cls(
@@ -39,6 +43,7 @@ class RelationType:
                 description=relation_type_dict['description'],
                 values=relation_type_dict['values'],
                 reflexive=relation_type_dict['reflexive'],
+                probabilistic=relation_type_dict['probabilistic'],
         )
 
     @classmethod
@@ -52,6 +57,7 @@ class RelationType:
             'description': self.description,
             'values': self.values,
             'reflexive': self.reflexive,
+            'probabilistic': self.probabilistic,
         }
         if include_id:
             relation_type_dict['_id'] = self.id
@@ -79,21 +85,21 @@ class Relation:
                         relation_type_id: ObjectId,
                         node_from_id: ObjectId,
                         node_to_id: ObjectId,
+                        probability: float = 1,
                         relation_id: Optional[ObjectId] = None,
                         ):
         if is_uni:
             return UniRelation(relation_type_id=relation_type_id,
                                node_from_id=node_from_id,
                                node_to_id=node_to_id,
+                               probability=probability,
                                relation_id=relation_id)
         else:
             return BiRelation(relation_type_id=relation_type_id,
                               node_1_id=node_from_id,
                               node_2_id=node_to_id,
+                              probability=probability,
                               relation_id=relation_id)
-
-
-
 
 
 class UniRelation:
@@ -102,6 +108,7 @@ class UniRelation:
                  relation_type_id: ObjectId,
                  node_from_id: ObjectId,
                  node_to_id: ObjectId,
+                 probability: float = 1,
                  relation_id: Optional[ObjectId] = None,
                  ):
 
@@ -109,7 +116,12 @@ class UniRelation:
         self.node_from = node_from_id
         self.node_to = node_to_id
         self.is_uni = True
+        self.probability = probability
         self.id = relation_id
+
+    @property
+    def certain(self) -> bool:
+        return abs(self.probability - 1.0) < EPSILON
 
     @property
     def node_1(self):
@@ -126,12 +138,14 @@ class UniRelation:
                 relation_type_id=relation_dict['type'],
                 node_from_id=relation_dict['node_from'],
                 node_to_id=relation_dict['node_to'],
-                relation_id=relation_dict['_id']
+                probability=relation_dict['probability'],
+                relation_id=relation_dict['_id'],
             )
         return cls(
             relation_type_id=relation_dict['type'],
             node_from_id=relation_dict['node_from'],
             node_to_id=relation_dict['node_to'],
+            probability=relation_dict['probability'],
         )
 
     def to_dict(self, include_id: bool = False) -> Dict:
@@ -139,7 +153,8 @@ class UniRelation:
             'type': self.type,
             'node_from': self.node_from,
             'node_to': self.node_to,
-            'is_uni': self.is_uni
+            'is_uni': self.is_uni,
+            'probability': self.probability,
         }
         if include_id:
             rel_dict['_id'] = self.id
@@ -153,14 +168,20 @@ class BiRelation:
                  relation_type_id: ObjectId,
                  node_1_id: ObjectId,
                  node_2_id: ObjectId,
+                 probability: float = 1,
                  relation_id: Optional[ObjectId] = None,
                  ):
         self.type = relation_type_id
         self.node_1 = node_1_id
         self.node_2 = node_2_id
+        self.probability = probability
 
         self.is_uni = False
         self.id = relation_id
+
+    @property
+    def certain(self) -> bool:
+        return abs(self.probability - 1.0) < EPSILON
 
     @classmethod
     def from_dict(cls, relation_dict: Dict):
@@ -169,12 +190,14 @@ class BiRelation:
                 relation_type_id=relation_dict['type'],
                 node_1_id=relation_dict['node_1'],
                 node_2_id=relation_dict['node_2'],
+                probability=relation_dict['probability'],
                 relation_id=relation_dict['_id']
             )
         return cls(
             relation_type_id=relation_dict['type'],
             node_1_id=relation_dict['node_1'],
             node_2_id=relation_dict['node_2'],
+            probability=relation_dict['probability'],
         )
 
     def to_dict(self, include_id: bool = False) -> Dict:
@@ -182,7 +205,8 @@ class BiRelation:
             'type': self.type,
             'node_1': self.node_1,
             'node_2': self.node_2,
-            'is_uni': self.is_uni
+            'is_uni': self.is_uni,
+            'probability': self.probability,
         }
         if include_id:
             rel_dict['_id'] = self.id
